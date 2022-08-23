d56247a51d9451030fb6afc75bb371da569ded5c
«alexander <favorite@bk.ru>
2022-08-23 16:29:44 +0300

    hw1

---
+++ HW_1/HW_01t/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+int b = a % 100;
+
+int c = b / 10;
+
+Console.WriteLine("Вторая цифра числа " + a + " -> " + c);
