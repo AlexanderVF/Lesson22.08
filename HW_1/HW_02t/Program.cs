96c69e6f42f3ae7f44cd52d51ddd30baae7d9b30
«alexander <favorite@bk.ru>
2022-08-23 22:08:39 +0300

    hw2

---
+++ HW_1/HW_02t/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(a>99 ? a.ToString()[2] : + a +" -> Нет третьей цифры");
+
+if (a < 100)
+{
+  Console.WriteLine(a + " -> Нет третьей цифры");  
+}
+else
+{
+    Console.WriteLine(a.ToString()[2]);
+}
+
---
+++ HW_3/Program.cs
@@ -0,0 +1,3 @@
+﻿int n = 23456;
+        Console.WriteLine(n>99 ? n.ToString()[2] : '-');
+        Console.ReadKey(true);
