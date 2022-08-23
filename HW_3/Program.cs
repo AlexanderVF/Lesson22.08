3e81857339db76347804e95b930ae77a76fd9d21
«alexander <favorite@bk.ru>
2022-08-23 22:29:20 +0300

    hw3

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,3 +1,40 @@
-﻿int n = 23456;
-        Console.WriteLine(n>99 ? n.ToString()[2] : '-');
-        Console.ReadKey(true);
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+int b = 1;
+int c = 2;
+int d = 3;
+int e = 4;
+int f = 5;
+int g = 6;
+int j = 7;
+
+if (a == b)
+{
+    Console.WriteLine(a + " -> Нет, рабочий день");
+}
+if (a == c)
+{
+    Console.WriteLine(a + " -> Нет, рабочий день");
+}
+if (a == d)
+{
+    Console.WriteLine(a + " -> Нет, рабочий день");
+}
+if (a == e)
+{
+    Console.WriteLine(a + " -> Нет, рабочий день");
+}
+if (a == f)
+{
+    Console.WriteLine(a + " -> Нет, рабочий день");
+}
+if (a == g)
+{
+    Console.WriteLine(a + " -> Да, выходной день");
+}
+if (a == j)
+{
+    Console.WriteLine(a + " -> Да, выходной день");
+}
