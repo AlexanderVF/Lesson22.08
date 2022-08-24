6c4d57523c4c308d4ce3ab46a8bca22fa2b32cb3
«alexander <favorite@bk.ru>
2022-08-24 20:52:18 +0300

    hw2_1

--- HW_1/HW_02t/Program.cs
+++ HW_1/HW_02t/Program.cs
@@ -3,13 +3,16 @@ Console.WriteLine("Введите число");
 int a = int.Parse(Console.ReadLine());
 
 //Console.WriteLine(a>99 ? a.ToString()[2] : + a +" -> Нет третьей цифры");
-
+if (a < 0)
+{
+    a = a * (-1);
+}
 if (a < 100)
 {
   Console.WriteLine(a + " -> Нет третьей цифры");  
 }
 else
 {
-    Console.WriteLine(a.ToString()[2]);
+    Console.WriteLine(a.ToString()[2] + " -> Третья цифра");
 }
 
